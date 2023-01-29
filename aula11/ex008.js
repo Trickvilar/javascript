a450091cff013a43225b6909f8cc0f3db3c82382
Trickvilar <99681658+Trickvilar@users.noreply.github.com>
2023-01-29 00:59:29 -0300

    criei os exercicios 08 09 e 010

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60
+
+if(vel > 50){  //CONDIÇÃO SIMPLES
+    console.log(`Você passou a ${vel}KM/h. Isto é acima do permitido. MULTADO!`)
+}
+console.log('Sempre ao dirigir, use cinto de segurança!')
---
+++ aula11/ex009.js
@@ -0,0 +1,9 @@
+var pais = 'França'
+
+if(pais == 'Brasil'){
+    console.log(`Vivendo em ${pais}`)
+    console.log('Você é Brasileiro')
+}else{
+    console.log(`Vivendo em ${pais}`)
+    console.log('Você é Estrangeiro')
+}
