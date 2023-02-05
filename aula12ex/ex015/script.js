3a278f7e4714ece7d025d712f00382e494da9607
Trickvilar <99681658+Trickvilar@users.noreply.github.com>
2023-02-05 14:50:52 -0300

    criado o ex 015

---
+++ aula12ex/ex015/script.js
@@ -0,0 +1,11 @@
+function verificar(){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.querySelector('#txtano')
+    var res = document.querySelector('#res')
+    if(fano.value.length == 0 || fano.value > ano){
+        alert('Verifique os dados e tente novamente')
+    }else{
+        alert('Tudo Ok!')
+    }
+}
